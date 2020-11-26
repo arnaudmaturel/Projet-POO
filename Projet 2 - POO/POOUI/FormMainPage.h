8a425856a1fa51ee081c5934248bd6290edf439d
NewKoaal <arnaud.merken@gmail.com>
2020-11-26 13:50:55 +0100

    UI - Gestion du personnel

--- Projet 2 - POO/POOUI/FormMainPage.h
+++ Projet 2 - POO/POOUI/FormMainPage.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "stdafx.h"
+
 namespace POOUI {
 
 	using namespace System;
@@ -45,6 +47,7 @@ namespace POOUI {
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::Label^ label4;
 	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
 
 	private:
 		/// <summary>
@@ -70,21 +73,21 @@ namespace POOUI {
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.Image")));
-			this->button1->Location = System::Drawing::Point(31, 126);
+			this->button1->Location = System::Drawing::Point(31, 157);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(138, 300);
 			this->button1->TabIndex = 0;
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button2.Image")));
-			this->button2->Location = System::Drawing::Point(196, 126);
+			this->button2->Location = System::Drawing::Point(196, 157);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(138, 300);
 			this->button2->TabIndex = 1;
@@ -92,8 +95,7 @@ namespace POOUI {
 			// 
 			// button3
 			// 
-			this->button3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.Image")));
-			this->button3->Location = System::Drawing::Point(364, 126);
+			this->button3->Location = System::Drawing::Point(364, 157);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(138, 300);
 			this->button3->TabIndex = 2;
@@ -101,8 +103,7 @@ namespace POOUI {
 			// 
 			// button4
 			// 
-			this->button4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button4.Image")));
-			this->button4->Location = System::Drawing::Point(528, 126);
+			this->button4->Location = System::Drawing::Point(528, 157);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(138, 300);
 			this->button4->TabIndex = 3;
@@ -110,8 +111,7 @@ namespace POOUI {
 			// 
 			// button5
 			// 
-			this->button5->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button5.Image")));
-			this->button5->Location = System::Drawing::Point(687, 126);
+			this->button5->Location = System::Drawing::Point(687, 157);
 			this->button5->Name = L"button5";
 			this->button5->Size = System::Drawing::Size(138, 300);
 			this->button5->TabIndex = 4;
@@ -122,7 +122,7 @@ namespace POOUI {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Montserrat", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(43, 70);
+			this->label1->Location = System::Drawing::Point(43, 101);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(126, 23);
 			this->label1->TabIndex = 5;
@@ -133,7 +133,7 @@ namespace POOUI {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Montserrat", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(224, 70);
+			this->label2->Location = System::Drawing::Point(224, 101);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(83, 23);
 			this->label2->TabIndex = 6;
@@ -144,7 +144,7 @@ namespace POOUI {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Montserrat", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(394, 70);
+			this->label3->Location = System::Drawing::Point(394, 101);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(80, 23);
 			this->label3->TabIndex = 7;
@@ -155,7 +155,7 @@ namespace POOUI {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Montserrat", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(551, 70);
+			this->label4->Location = System::Drawing::Point(551, 101);
 			this->label4->Name = L"label4";
 			this->label4->Size = System::Drawing::Size(91, 23);
 			this->label4->TabIndex = 8;
@@ -166,11 +166,23 @@ namespace POOUI {
 			this->label5->AutoSize = true;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Montserrat", 14.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label5->Location = System::Drawing::Point(683, 70);
+			this->label5->Location = System::Drawing::Point(683, 101);
 			this->label5->Name = L"label5";
 			this->label5->Size = System::Drawing::Size(136, 23);
 			this->label5->TabIndex = 9;
 			this->label5->Text = L"Statistiques";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Montserrat", 36, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(318, 9);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(217, 58);
+			this->label6->TabIndex = 10;
+			this->label6->Text = L"Gestion";
+			this->label6->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// MyForm
 			// 
@@ -178,6 +190,7 @@ namespace POOUI {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::AliceBlue;
 			this->ClientSize = System::Drawing::Size(854, 480);
+			this->Controls->Add(this->label6);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->label3);
@@ -195,5 +208,11 @@ namespace POOUI {
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Hide();
+		POOUI::MyFormPersonnel GestionPersonnel;
+		GestionPersonnel.ShowDialog();
+		this->Show();
+	}
+};
 }
--- Projet 2 - POO/Projet 2 - POO/Source.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-
-int main()
-{
-	CLemploye* e1 = new CLemploye;
-
-	e1->consulter();
-	e1->ajouter("");
-}
