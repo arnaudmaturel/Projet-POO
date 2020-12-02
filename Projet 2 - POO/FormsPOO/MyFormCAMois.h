a4f251aa10763fa04994b32b0864fb99fb554e81
Maëlys RAVENEL <maelys.ravenel@viacesi.fr>
2020-12-02 17:22:54 +0100

    CA FONCTIONNE PAS, JE SAIS

--- Projet 2 - POO/FormsPOO/MyFormCAMois.h
+++ Projet 2 - POO/FormsPOO/MyFormCAMois.h
@@ -8,6 +8,7 @@ namespace FormsPOO {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
 
 	/// <summary>
 	/// Description résumée de MyFormCAMois
@@ -38,15 +39,20 @@ namespace FormsPOO {
 	protected:
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label3;
-	private: System::Windows::Forms::ListBox^ Month_list;
+
 	private: System::Windows::Forms::TextBox^ textBox1;
-	private: System::Windows::Forms::TextBox^ textBox2;
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::BindingSource^ bindingSource1;
+	private: System::Windows::Forms::ListBox^ listBox1;
+
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Variable nécessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -55,12 +61,15 @@ namespace FormsPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->Month_list = (gcnew System::Windows::Forms::ListBox());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->bindingSource1 = (gcnew System::Windows::Forms::BindingSource(this->components));
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -79,7 +88,7 @@ namespace FormsPOO {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(40, 157);
+			this->label2->Location = System::Drawing::Point(342, 170);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(96, 32);
 			this->label2->TabIndex = 1;
@@ -90,53 +99,82 @@ namespace FormsPOO {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(40, 223);
+			this->label3->Location = System::Drawing::Point(342, 236);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(120, 32);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Année: ";
 			// 
-			// Month_list
-			// 
-			this->Month_list->FormattingEnabled = true;
-			this->Month_list->ItemHeight = 16;
-			this->Month_list->Location = System::Drawing::Point(175, 166);
-			this->Month_list->Name = L"Month_list";
-			this->Month_list->Size = System::Drawing::Size(191, 20);
-			this->Month_list->TabIndex = 3;
-			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(176, 230);
+			this->textBox1->Location = System::Drawing::Point(537, 246);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(199, 22);
 			this->textBox1->TabIndex = 4;
 			// 
-			// textBox2
+			// button1
 			// 
-			this->textBox2->Location = System::Drawing::Point(604, 191);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(316, 22);
-			this->textBox2->TabIndex = 5;
-			this->textBox2->Text = L"*display*";
+			this->button1->Location = System::Drawing::Point(454, 331);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(163, 32);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"Calculer";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyFormCAMois::button1_Click);
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Items->AddRange(gcnew cli::array< System::Object^  >(12) {
+				L"Janvier", L"Février", L"Mars", L"Avril", L"Mai",
+					L"Juin", L"Juillet", L"Août", L"Septembre", L"Octobre", L"Novembre", L"Décembre"
+			});
+			this->listBox1->Location = System::Drawing::Point(533, 170);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(203, 20);
+			this->listBox1->TabIndex = 7;
 			// 
 			// MyFormCAMois
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1034, 557);
-			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
-			this->Controls->Add(this->Month_list);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Name = L"MyFormCAMois";
 			this->Text = L"MyFormCAMois";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try {
+			String^ constr = "Server=127.0.0.1;Uid=root;Pwd=;Database=projetpoo";
+			MySqlConnection^ con = gcnew MySqlConnection(constr);
+
+			int Year = Int32::Parse(textBox1->/**/);
+			int Month =;
+
+			//requête: 
+			if (Month == " ")
+			{
+
+			}
+			MySqlCommand^ cmd = gcnew MySqlCommand("SELECT SUM(paiement.MONTANT_PAIEMENT) AS ChiffeDAffaire FROM paiement WHERE paiement.DATE_PAIEMENT BETWEEN"+/*AAAA+"-"+MM*/+"-"+DayB+"AND"AAAA"+"-"+"MM"-"+ DayE, con);
+			MySqlDataReader^ dr;
+
+			con->Open();
+			dr = cmd->ExecuteReader();
+			//MessageBox::Show(contenu);
+			con->Close();
+		}
+	}
 };
 }
--- Projet 2 - POO/FormsPOO/MyFormClient.h
+++ Projet 2 - POO/FormsPOO/MyFormClient.h
@@ -121,54 +121,59 @@ namespace FormsPOOclient {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Montserrat", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(266, 106);
+			this->label1->Location = System::Drawing::Point(355, 130);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(63, 33);
+			this->label1->Size = System::Drawing::Size(74, 39);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"ID :";
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Montserrat", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(226, 148);
+			this->label2->Location = System::Drawing::Point(301, 182);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(103, 33);
+			this->label2->Size = System::Drawing::Size(112, 39);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Nom :";
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Montserrat", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(183, 187);
+			this->label3->Location = System::Drawing::Point(244, 230);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(146, 33);
+			this->label3->Size = System::Drawing::Size(162, 39);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Prenom :";
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Font = (gcnew System::Drawing::Font(L"Montserrat", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(77, 226);
+			this->label4->Location = System::Drawing::Point(103, 278);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(252, 33);
+			this->label4->Size = System::Drawing::Size(286, 39);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"Date naissance :";
 			// 
 			// button1
 			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(12, 466);
+			this->button1->Location = System::Drawing::Point(16, 574);
+			this->button1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(143, 36);
+			this->button1->Size = System::Drawing::Size(191, 44);
 			this->button1->TabIndex = 7;
 			this->button1->Text = L"Ajouter";
 			this->button1->UseVisualStyleBackColor = true;
@@ -176,11 +181,12 @@ namespace FormsPOOclient {
 			// 
 			// button2
 			// 
-			this->button2->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(161, 466);
+			this->button2->Location = System::Drawing::Point(215, 574);
+			this->button2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(143, 36);
+			this->button2->Size = System::Drawing::Size(191, 44);
 			this->button2->TabIndex = 8;
 			this->button2->Text = L"Modifier";
 			this->button2->UseVisualStyleBackColor = true;
@@ -188,11 +194,12 @@ namespace FormsPOOclient {
 			// 
 			// button3
 			// 
-			this->button3->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(310, 466);
+			this->button3->Location = System::Drawing::Point(413, 574);
+			this->button3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(143, 36);
+			this->button3->Size = System::Drawing::Size(191, 44);
 			this->button3->TabIndex = 9;
 			this->button3->Text = L"Supprimer";
 			this->button3->UseVisualStyleBackColor = true;
@@ -200,11 +207,12 @@ namespace FormsPOOclient {
 			// 
 			// button4
 			// 
-			this->button4->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button4->Location = System::Drawing::Point(621, 59);
+			this->button4->Location = System::Drawing::Point(828, 73);
+			this->button4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(176, 36);
+			this->button4->Size = System::Drawing::Size(235, 44);
 			this->button4->TabIndex = 10;
 			this->button4->Text = L"Chercher";
 			this->button4->UseVisualStyleBackColor = true;
@@ -212,11 +220,12 @@ namespace FormsPOOclient {
 			// 
 			// button5
 			// 
-			this->button5->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button5->Location = System::Drawing::Point(803, 59);
+			this->button5->Location = System::Drawing::Point(1071, 73);
+			this->button5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(176, 36);
+			this->button5->Size = System::Drawing::Size(235, 44);
 			this->button5->TabIndex = 11;
 			this->button5->Text = L"Afficher tout";
 			this->button5->UseVisualStyleBackColor = true;
@@ -225,27 +234,30 @@ namespace FormsPOOclient {
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(622, 107);
+			this->dataGridView1->Location = System::Drawing::Point(829, 132);
+			this->dataGridView1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(540, 395);
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->Size = System::Drawing::Size(720, 486);
 			this->dataGridView1->TabIndex = 12;
 			// 
 			// menuStrip1
 			// 
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
 				this->acceuilToolStripMenuItem,
 					this->menuToolStripMenuItem
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(1174, 24);
+			this->menuStrip1->Size = System::Drawing::Size(1565, 28);
 			this->menuStrip1->TabIndex = 13;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
 			// acceuilToolStripMenuItem
 			// 
 			this->acceuilToolStripMenuItem->Name = L"acceuilToolStripMenuItem";
-			this->acceuilToolStripMenuItem->Size = System::Drawing::Size(58, 20);
+			this->acceuilToolStripMenuItem->Size = System::Drawing::Size(71, 24);
 			this->acceuilToolStripMenuItem->Text = L"Acceuil";
 			this->acceuilToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyFormClient::acceuilToolStripMenuItem_Click);
 			// 
@@ -256,96 +268,103 @@ namespace FormsPOOclient {
 					this->gestionDesArticlesToolStripMenuItem, this->gestionDuStockToolStripMenuItem, this->gestionDesStatistiquesToolStripMenuItem
 			});
 			this->menuToolStripMenuItem->Name = L"menuToolStripMenuItem";
-			this->menuToolStripMenuItem->Size = System::Drawing::Size(50, 20);
+			this->menuToolStripMenuItem->Size = System::Drawing::Size(60, 24);
 			this->menuToolStripMenuItem->Text = L"Menu";
 			// 
 			// gestionDesClientsToolStripMenuItem
 			// 
 			this->gestionDesClientsToolStripMenuItem->Name = L"gestionDesClientsToolStripMenuItem";
-			this->gestionDesClientsToolStripMenuItem->Size = System::Drawing::Size(197, 22);
+			this->gestionDesClientsToolStripMenuItem->Size = System::Drawing::Size(247, 26);
 			this->gestionDesClientsToolStripMenuItem->Text = L"Gestion du personnel";
 			// 
 			// gestionDesArticlesToolStripMenuItem
 			// 
 			this->gestionDesArticlesToolStripMenuItem->Name = L"gestionDesArticlesToolStripMenuItem";
-			this->gestionDesArticlesToolStripMenuItem->Size = System::Drawing::Size(197, 22);
+			this->gestionDesArticlesToolStripMenuItem->Size = System::Drawing::Size(247, 26);
 			this->gestionDesArticlesToolStripMenuItem->Text = L"Gestion des articles";
 			// 
 			// gestionDuStockToolStripMenuItem
 			// 
 			this->gestionDuStockToolStripMenuItem->Name = L"gestionDuStockToolStripMenuItem";
-			this->gestionDuStockToolStripMenuItem->Size = System::Drawing::Size(197, 22);
+			this->gestionDuStockToolStripMenuItem->Size = System::Drawing::Size(247, 26);
 			this->gestionDuStockToolStripMenuItem->Text = L"Gestion du stock ";
 			// 
 			// gestionDesStatistiquesToolStripMenuItem
 			// 
 			this->gestionDesStatistiquesToolStripMenuItem->Name = L"gestionDesStatistiquesToolStripMenuItem";
-			this->gestionDesStatistiquesToolStripMenuItem->Size = System::Drawing::Size(197, 22);
+			this->gestionDesStatistiquesToolStripMenuItem->Size = System::Drawing::Size(247, 26);
 			this->gestionDesStatistiquesToolStripMenuItem->Text = L"Gestion des statistiques";
 			// 
 			// label8
 			// 
 			this->label8->AutoSize = true;
-			this->label8->Font = (gcnew System::Drawing::Font(L"Montserrat", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label8->Location = System::Drawing::Point(16, 265);
+			this->label8->Location = System::Drawing::Point(21, 326);
+			this->label8->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(313, 33);
+			this->label8->Size = System::Drawing::Size(343, 39);
 			this->label8->TabIndex = 14;
 			this->label8->Text = L"Date premier achat :";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox1->Location = System::Drawing::Point(335, 109);
+			this->textBox1->Location = System::Drawing::Point(447, 134);
+			this->textBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(259, 33);
+			this->textBox1->Size = System::Drawing::Size(344, 37);
 			this->textBox1->TabIndex = 15;
 			// 
 			// textBox2
 			// 
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox2->Location = System::Drawing::Point(335, 148);
+			this->textBox2->Location = System::Drawing::Point(447, 182);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(259, 33);
+			this->textBox2->Size = System::Drawing::Size(344, 37);
 			this->textBox2->TabIndex = 16;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox3->Location = System::Drawing::Point(335, 187);
+			this->textBox3->Location = System::Drawing::Point(447, 230);
+			this->textBox3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(259, 33);
+			this->textBox3->Size = System::Drawing::Size(344, 37);
 			this->textBox3->TabIndex = 17;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBox4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox4->Location = System::Drawing::Point(335, 226);
+			this->textBox4->Location = System::Drawing::Point(447, 278);
+			this->textBox4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(259, 33);
+			this->textBox4->Size = System::Drawing::Size(344, 37);
 			this->textBox4->TabIndex = 18;
 			// 
 			// textBox5
 			// 
-			this->textBox5->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->textBox5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox5->Location = System::Drawing::Point(335, 265);
+			this->textBox5->Location = System::Drawing::Point(447, 326);
+			this->textBox5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(259, 33);
+			this->textBox5->Size = System::Drawing::Size(344, 37);
 			this->textBox5->TabIndex = 19;
 			// 
 			// button6
 			// 
-			this->button6->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button6->Location = System::Drawing::Point(335, 304);
+			this->button6->Location = System::Drawing::Point(447, 374);
+			this->button6->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(259, 36);
+			this->button6->Size = System::Drawing::Size(345, 44);
 			this->button6->TabIndex = 20;
 			this->button6->Text = L"Gérer les adresses";
 			this->button6->UseVisualStyleBackColor = true;
@@ -354,19 +373,20 @@ namespace FormsPOOclient {
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Font = (gcnew System::Drawing::Font(L"Montserrat", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label5->Location = System::Drawing::Point(179, 304);
+			this->label5->Location = System::Drawing::Point(239, 374);
+			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(150, 33);
+			this->label5->Size = System::Drawing::Size(169, 39);
 			this->label5->TabIndex = 21;
 			this->label5->Text = L"Adresse :";
 			// 
 			// MyFormClient
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1174, 514);
+			this->ClientSize = System::Drawing::Size(1565, 633);
 			this->Controls->Add(this->label5);
 			this->Controls->Add(this->button6);
 			this->Controls->Add(this->textBox5);
@@ -387,6 +407,7 @@ namespace FormsPOOclient {
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->menuStrip1);
 			this->MainMenuStrip = this->menuStrip1;
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"MyFormClient";
 			this->Text = L"Gestion des clients";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
