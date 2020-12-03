c70d2088e9be6b90b5c354be5f36cf89d18e6f23
Maëlys RAVENEL <maelys.ravenel@viacesi.fr>
2020-12-03 14:50:09 +0100

    Panier moyen, ca s'affiche pas

--- Projet 2 - POO/FormsPOO/MyFormPanier_moy.h
+++ Projet 2 - POO/FormsPOO/MyFormPanier_moy.h
@@ -8,6 +8,8 @@ namespace FormsPOO {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
+	using namespace MySql::Data::MySqlClient;
+
 
 	/// <summary>
 	/// Description résumée de MyFormPanier_moy
@@ -35,14 +37,18 @@ namespace FormsPOO {
 			}
 		}
 	private: System::Windows::Forms::Label^ Label_text;
+	private: System::Windows::Forms::BindingSource^ bindingSource1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::ComponentModel::IContainer^ components;
 	protected:
-	private: System::Windows::Forms::TextBox^ textBox1;
+
 
 	private:
 		/// <summary>
 		/// Variable nécessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -51,8 +57,13 @@ namespace FormsPOO {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
 			this->Label_text = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->bindingSource1 = (gcnew System::Windows::Forms::BindingSource(this->components));
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// Label_text
@@ -67,27 +78,59 @@ namespace FormsPOO {
 			this->Label_text->TabIndex = 0;
 			this->Label_text->Text = L"Le prix moyen d\'un panier est de: ";
 			// 
-			// textBox1
+			// dataGridView1
 			// 
-			this->textBox1->Location = System::Drawing::Point(320, 241);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(405, 22);
-			this->textBox1->TabIndex = 1;
-			this->textBox1->Text = L"*display*";
+			this->dataGridView1->AutoGenerateColumns = false;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->DataSource = this->bindingSource1;
+			this->dataGridView1->Location = System::Drawing::Point(321, 157);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(384, 131);
+			this->dataGridView1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(327, 332);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(377, 50);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Afficher";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyFormPanier_moy::button1_Click);
 			// 
 			// MyFormPanier_moy
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1015, 571);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->Label_text);
 			this->Name = L"MyFormPanier_moy";
 			this->Text = L"Prix moyen d\'un panier";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try {
+			String^ constr = "Server=127.0.0.1;Uid=root;Pwd=;Database=projetpoo";
+			MySqlConnection^ con = gcnew MySqlConnection(constr);
+
+			MySqlDataAdapter^ sda = gcnew MySqlDataAdapter("SELECT AVG(commande.PRIX_TOTAL_TTC) AS PrixMoyenPanier FROM commande", con);
+			DataTable^ dt = gcnew DataTable();
+			sda->Fill(dt);
+			bindingSource1->DataSource = dt;
+			dataGridView1->DataSource = bindingSource1;
+		}
+		catch (Exception^ ex) {
+			MessageBox::Show(ex->Message);
+		}
+	}
+};
 }
