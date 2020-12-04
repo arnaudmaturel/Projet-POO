dd4abf32c79419569bcbb27458355f6b97e9669f
NewKoaal <arnaud.merken@gmail.com>
2020-12-04 10:12:13 +0100

    Problème échelle

--- Projet 2 - POO/FormsPOO/MyFormClient.h
+++ Projet 2 - POO/FormsPOO/MyFormClient.h
@@ -110,10 +110,9 @@ namespace FormsPOOclient {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(355, 130);
-			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label1->Location = System::Drawing::Point(267, 61);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(74, 39);
+			this->label1->Size = System::Drawing::Size(61, 31);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"ID :";
 			// 
@@ -122,10 +121,9 @@ namespace FormsPOOclient {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(301, 182);
-			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label2->Location = System::Drawing::Point(227, 103);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(112, 39);
+			this->label2->Size = System::Drawing::Size(91, 31);
 			this->label2->TabIndex = 1;
 			this->label2->Text = L"Nom :";
 			// 
@@ -134,10 +132,9 @@ namespace FormsPOOclient {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(244, 230);
-			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label3->Location = System::Drawing::Point(184, 142);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(162, 39);
+			this->label3->Size = System::Drawing::Size(131, 31);
 			this->label3->TabIndex = 2;
 			this->label3->Text = L"Prenom :";
 			// 
@@ -146,10 +143,9 @@ namespace FormsPOOclient {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(103, 278);
-			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label4->Location = System::Drawing::Point(78, 181);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(286, 39);
+			this->label4->Size = System::Drawing::Size(233, 31);
 			this->label4->TabIndex = 3;
 			this->label4->Text = L"Date naissance :";
 			// 
@@ -157,9 +153,10 @@ namespace FormsPOOclient {
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(12, 499);
+			this->button1->Location = System::Drawing::Point(11, 409);
+			this->button1->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(191, 44);
+			this->button1->Size = System::Drawing::Size(143, 36);
 			this->button1->TabIndex = 7;
 			this->button1->Text = L"Ajouter";
 			this->button1->UseVisualStyleBackColor = true;
@@ -169,9 +166,10 @@ namespace FormsPOOclient {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(161, 499);
+			this->button2->Location = System::Drawing::Point(158, 409);
+			this->button2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(191, 44);
+			this->button2->Size = System::Drawing::Size(143, 36);
 			this->button2->TabIndex = 8;
 			this->button2->Text = L"Modifier";
 			this->button2->UseVisualStyleBackColor = true;
@@ -181,9 +179,10 @@ namespace FormsPOOclient {
 			// 
 			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(310, 499);
+			this->button3->Location = System::Drawing::Point(305, 409);
+			this->button3->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(191, 44);
+			this->button3->Size = System::Drawing::Size(143, 36);
 			this->button3->TabIndex = 9;
 			this->button3->Text = L"Supprimer";
 			this->button3->UseVisualStyleBackColor = true;
@@ -193,10 +192,9 @@ namespace FormsPOOclient {
 			// 
 			this->button4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button4->Location = System::Drawing::Point(828, 73);
-			this->button4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button4->Location = System::Drawing::Point(621, 16);
 			this->button4->Name = L"button4";
-			this->button4->Size = System::Drawing::Size(235, 44);
+			this->button4->Size = System::Drawing::Size(176, 36);
 			this->button4->TabIndex = 10;
 			this->button4->Text = L"Chercher";
 			this->button4->UseVisualStyleBackColor = true;
@@ -206,10 +204,9 @@ namespace FormsPOOclient {
 			// 
 			this->button5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button5->Location = System::Drawing::Point(1071, 73);
-			this->button5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button5->Location = System::Drawing::Point(803, 16);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(235, 44);
+			this->button5->Size = System::Drawing::Size(176, 36);
 			this->button5->TabIndex = 11;
 			this->button5->Text = L"Afficher tout";
 			this->button5->UseVisualStyleBackColor = true;
@@ -218,10 +215,9 @@ namespace FormsPOOclient {
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(829, 132);
-			this->dataGridView1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->dataGridView1->Location = System::Drawing::Point(622, 64);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(540, 233);
+			this->dataGridView1->Size = System::Drawing::Size(405, 231);
 			this->dataGridView1->TabIndex = 12;
 			// 
 			// label8
@@ -229,10 +225,9 @@ namespace FormsPOOclient {
 			this->label8->AutoSize = true;
 			this->label8->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label8->Location = System::Drawing::Point(21, 326);
-			this->label8->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label8->Location = System::Drawing::Point(17, 220);
 			this->label8->Name = L"label8";
-			this->label8->Size = System::Drawing::Size(343, 39);
+			this->label8->Size = System::Drawing::Size(279, 31);
 			this->label8->TabIndex = 14;
 			this->label8->Text = L"Date premier achat :";
 			// 
@@ -240,62 +235,56 @@ namespace FormsPOOclient {
 			// 
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox1->Location = System::Drawing::Point(447, 134);
-			this->textBox1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->textBox1->Location = System::Drawing::Point(336, 64);
 			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(344, 37);
+			this->textBox1->Size = System::Drawing::Size(259, 31);
 			this->textBox1->TabIndex = 15;
 			// 
 			// textBox2
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox2->Location = System::Drawing::Point(447, 182);
-			this->textBox2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->textBox2->Location = System::Drawing::Point(336, 103);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(344, 37);
+			this->textBox2->Size = System::Drawing::Size(259, 31);
 			this->textBox2->TabIndex = 16;
 			// 
 			// textBox3
 			// 
 			this->textBox3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox3->Location = System::Drawing::Point(447, 230);
-			this->textBox3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->textBox3->Location = System::Drawing::Point(336, 142);
 			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(344, 37);
+			this->textBox3->Size = System::Drawing::Size(259, 31);
 			this->textBox3->TabIndex = 17;
 			// 
 			// textBox4
 			// 
 			this->textBox4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox4->Location = System::Drawing::Point(447, 278);
-			this->textBox4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->textBox4->Location = System::Drawing::Point(336, 181);
 			this->textBox4->Name = L"textBox4";
-			this->textBox4->Size = System::Drawing::Size(344, 37);
+			this->textBox4->Size = System::Drawing::Size(259, 31);
 			this->textBox4->TabIndex = 18;
 			// 
 			// textBox5
 			// 
 			this->textBox5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->textBox5->Location = System::Drawing::Point(447, 326);
-			this->textBox5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->textBox5->Location = System::Drawing::Point(336, 220);
 			this->textBox5->Name = L"textBox5";
-			this->textBox5->Size = System::Drawing::Size(344, 37);
+			this->textBox5->Size = System::Drawing::Size(259, 31);
 			this->textBox5->TabIndex = 19;
 			// 
 			// button6
 			// 
 			this->button6->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button6->Location = System::Drawing::Point(447, 374);
-			this->button6->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->button6->Location = System::Drawing::Point(336, 259);
 			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(345, 44);
+			this->button6->Size = System::Drawing::Size(259, 36);
 			this->button6->TabIndex = 20;
-			this->button6->Text = L"G?rer les adresses";
+			this->button6->Text = L"Gérer les adresses";
 			this->button6->UseVisualStyleBackColor = true;
 			this->button6->Click += gcnew System::EventHandler(this, &MyFormClient::button6_Click);
 			// 
@@ -304,19 +293,19 @@ namespace FormsPOOclient {
 			this->label5->AutoSize = true;
 			this->label5->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label5->Location = System::Drawing::Point(239, 374);
-			this->label5->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
+			this->label5->Location = System::Drawing::Point(180, 259);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(169, 39);
+			this->label5->Size = System::Drawing::Size(138, 31);
 			this->label5->TabIndex = 21;
 			this->label5->Text = L"Adresse :";
 			// 
 			// dataGridView2
 			// 
 			this->dataGridView2->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView2->Location = System::Drawing::Point(622, 385);
+			this->dataGridView2->Location = System::Drawing::Point(622, 323);
+			this->dataGridView2->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->dataGridView2->Name = L"dataGridView2";
-			this->dataGridView2->Size = System::Drawing::Size(540, 150);
+			this->dataGridView2->Size = System::Drawing::Size(405, 122);
 			this->dataGridView2->TabIndex = 22;
 			// 
 			// label6
@@ -324,7 +313,8 @@ namespace FormsPOOclient {
 			this->label6->AutoSize = true;
 			this->label6->Font = (gcnew System::Drawing::Font(L"Montserrat", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label6->Location = System::Drawing::Point(617, 357);
+			this->label6->Location = System::Drawing::Point(617, 296);
+			this->label6->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label6->Name = L"label6";
 			this->label6->Size = System::Drawing::Size(241, 25);
 			this->label6->TabIndex = 23;
@@ -332,9 +322,9 @@ namespace FormsPOOclient {
 			// 
 			// MyFormClient
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1174, 547);
+			this->ClientSize = System::Drawing::Size(1046, 453);
 			this->Controls->Add(this->label6);
 			this->Controls->Add(this->dataGridView2);
 			this->Controls->Add(this->label5);
@@ -355,6 +345,7 @@ namespace FormsPOOclient {
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(2, 2, 2, 2);
 			this->Name = L"MyFormClient";
 			this->Text = L"Gestion des clients";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
